e643e21cc072da2192336b2a0ca7cdc47bf96cfc
ColonolNutty <ColonolNutty@hotmail.com>
2019-09-07 19:37:41 -0600

    Add a way to register interactions to terrain or objects

---
+++ Scripts/sims4communitylib/services/interactions/__init__.py
@@ -0,0 +1,7 @@
+"""
+This file is part of the Sims 4 Community Library, licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International public license (CC BY-NC-ND 4.0).
+https://creativecommons.org/licenses/by-nc-nd/4.0/
+https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode
+
+Copyright (c) COLONOLNUTTY
+"""
