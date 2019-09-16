60c62d779f098b581a81abe723ba5c6759279a2c
ColonolNutty <ColonolNutty@hotmail.com>
2019-09-15 18:48:17 -0600

    Add a utility to get all nearby sims

--- Scripts/sims4communitylib/utils/sims/common_sim_utils.py
+++ Scripts/sims4communitylib/utils/sims/common_sim_utils.py
@@ -6,8 +6,11 @@ https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode
 Copyright (c) COLONOLNUTTY
 """
 import services
+import sims4.commands
+from typing import Iterator, Callable
 from sims.sim import Sim
 from sims.sim_info import SimInfo
+from objects import ALL_HIDDEN_REASONS
 
 
 class CommonSimUtils:
@@ -16,7 +19,6 @@ class CommonSimUtils:
     def get_active_sim() -> Sim:
         """
             Retrieve a Sim object of the Currently Active Sim.
-        :return: The Sim object of the currently active sim.
         """
         client = services.client_manager().get_first_client()
         return client.active_sim
@@ -25,6 +27,49 @@ class CommonSimUtils:
     def get_active_sim_info() -> SimInfo:
         """
             Retrieve a SimInfo object of the Currently Active Sim.
-        :return: The SimInfo object of the currently active sim.
         """
         return CommonSimUtils.get_active_sim().sim_info
+
+    @staticmethod
+    def get_all_sims_generator(include_sim_callback: Callable[[SimInfo], bool]=None) -> Iterator[Sim]:
+        """
+            Retrieve a Sim object for each and every sim (including hidden sims).
+        :param include_sim_callback: If the result of this callback is True, the sim will be included in the results. All sims will be included if not provided.
+        """
+        for sim_info in CommonSimUtils.get_sim_info_for_all_sims_generator(include_sim_callback=include_sim_callback):
+            sim_instance = sim_info.get_sim_instance(allow_hidden_flags=ALL_HIDDEN_REASONS)
+            if sim_instance is None:
+                continue
+            yield sim_instance
+
+    @staticmethod
+    def get_sim_info_for_all_sims_generator(include_sim_callback: Callable[[SimInfo], bool]=None) -> Iterator[SimInfo]:
+        """
+            Retrieve a SimInfo object for each and every sim.
+        :param include_sim_callback: If the result of this callback is True, the sim will be included in the results. All sims will be included if not provided.
+        """
+        sim_info_list = list(services.sim_info_manager().get_all())
+        for sim_info in sim_info_list:
+            if sim_info is None:
+                continue
+            if include_sim_callback is not None and include_sim_callback(sim_info) is False:
+                continue
+            yield sim_info
+
+
+@sims4.commands.Command('s4clib_testing.display_name_of_currently_active_sim', command_type=sims4.commands.CommandType.Live)
+def _common_testing_display_name_of_currently_active_sim(_connection: int=None):
+    output = sims4.commands.CheatOutput(_connection)
+    sim_info = CommonSimUtils.get_active_sim_info()
+    output('Currently Active Sim: {} {}'.format(sim_info.first_name, sim_info.last_name))
+
+
+@sims4.commands.Command('s4clib_testing.display_names_of_all_sims', command_type=sims4.commands.CommandType.Live)
+def _common_testing_display_names_of_all_sims(_connection: int=None):
+    output = sims4.commands.CheatOutput(_connection)
+    output('Showing the names of all sims (This may take awhile).')
+    current_count = 1
+    for sim_info in CommonSimUtils.get_sim_info_for_all_sims_generator():
+        output('{}: {} {}'.format(str(current_count), sim_info.first_name, sim_info.last_name))
+        current_count += 1
+    output('Done showing the names of all sims.')
