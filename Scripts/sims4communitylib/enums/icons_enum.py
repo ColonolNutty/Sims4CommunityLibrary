0a0e24ff46aa436a5d0a9cbb2aa3979427c427b2
ColonolNutty <ColonolNutty@hotmail.com>
2019-10-06 13:35:02 -0600

    Create and add icons for use with dialogs
    - Add CommonIconUtils for accessing the icons

---
+++ Scripts/sims4communitylib/enums/icons_enum.py
@@ -0,0 +1,18 @@
+"""
+This file is part of the Sims 4 Community Library, licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International public license (CC BY-NC-ND 4.0).
+https://creativecommons.org/licenses/by-nc-nd/4.0/
+https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode
+
+Copyright (c) COLONOLNUTTY
+"""
+from sims4communitylib.enums.enumtypes.int_enum import CommonEnumIntBase
+
+
+class CommonIconId(CommonEnumIntBase):
+    """ Identifiers for various vanilla and Sims 4 Community Library icons. """
+    S4CLIB_ARROW_RIGHT_ICON = 3589997896388655139
+    S4CLIB_ARROW_NAVIGATE_INTO_ICON = 17672018034831121750
+    S4CLIB_QUESTION_MARK_ICON = 16712671502182128037
+    S4CLIB_SIX_SIDED_DICE_ICON = 1749603411595915770
+    S4CLIB_CHECKED_SQUARE_ICON = 12632258471925492012
+    S4CLIB_UNCHECKED_SQUARE_ICON = 6160391714040730819
