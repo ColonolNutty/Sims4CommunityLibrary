a73c1cff9acbfd13687ab70f6588c8739f354c15
ColonolNutty <ColonolNutty@hotmail.com>
2019-10-06 12:09:51 -0600

    Add CommonTimeUtils
    - It will enable setting/getting the game time

--- Scripts/sims4communitylib/utils/sims/common_sim_state_utils.py
+++ Scripts/sims4communitylib/utils/sims/common_sim_state_utils.py
@@ -8,6 +8,7 @@ Copyright (c) COLONOLNUTTY
 from sims.sim_info import SimInfo
 from sims4communitylib.enums.buffs_enum import CommonBuffId
 from sims4communitylib.utils.sims.common_buff_utils import CommonBuffUtils
+from sims4communitylib.utils.sims.common_sim_utils import CommonSimUtils
 
 
 class CommonSimStateUtils:
@@ -25,3 +26,12 @@ class CommonSimStateUtils:
             Determine if a sim is currently wearing a towel.
         """
         return CommonBuffUtils.has_buff(sim_info, CommonBuffId.IS_WEARING_TOWEL)
+
+    @staticmethod
+    def is_in_sunlight(sim_info: SimInfo) -> bool:
+        """
+            Determine if a sim is in sunlight.
+        """
+        from sims4communitylib.utils.common_time_utils import CommonTimeUtils
+        sim = CommonSimUtils.get_sim_instance(sim_info)
+        return CommonTimeUtils.get_time_service().is_in_sunlight(sim)
