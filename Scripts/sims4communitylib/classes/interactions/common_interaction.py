10d14d88d3acaa1753dda380eb79c5d52c9bc911
ColonolNutty <ColonolNutty@hotmail.com>
2019-10-05 21:13:26 -0600

    Create an example interaction

--- Scripts/sims4communitylib/classes/interactions/common_interaction.py
+++ Scripts/sims4communitylib/classes/interactions/common_interaction.py
@@ -160,3 +160,27 @@ class CommonInteraction(Interaction):
         :return: Return a modified PostureState, ParticipantType, and Sim.
         """
         return posture_state, participant_type, sim
+
+
+# The following is an example interaction that varies when it will display, when it will be hidden, and when it will be disabled with a tooltip.
+class _ExampleInteraction(CommonInteraction):
+    @classmethod
+    def on_test(cls, interaction_sim: Sim, interaction_target: Any, interaction_context: InteractionContext, **kwargs) -> TestResult:
+        result = 1 + 1
+        if result == 2:
+            # Interaction will be displayed, but disabled, it will also have a tooltip that displays on hover with the text "Test Tooltip"
+            return cls.create_test_result(False, reason="Test Tooltip")
+            # Alternative way to specify a tooltip with the text "Test Tooltip"
+            # return cls.create_test_result(False, reason="No Reason", tooltip=CommonLocalizationUtils.create_localized_tooltip("Test Tooltip"))
+        if result == 3:
+            # Interaction will be hidden completely.
+            return TestResult.NONE
+        # Interaction will display and be enabled.
+        return TestResult.TRUE
+
+    def on_started(self, interaction_sim: Sim, interaction_target: Any) -> bool:
+        result = True
+        if not result:
+            return False
+        # Put here what you want the interaction to do as soon as the player clicks it while it is enabled.
+        return True
