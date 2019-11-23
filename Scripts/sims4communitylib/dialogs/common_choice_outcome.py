5817561344c3c4064109f93f8c0d1d50d2c96740
ColonolNutty <ColonolNutty@hotmail.com>
2019-11-23 13:42:07 -0700

    Add an object dialog with pagination.

---
+++ Scripts/sims4communitylib/dialogs/common_choice_outcome.py
@@ -0,0 +1,21 @@
+"""
+This file is part of the Sims 4 Community Library, licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International public license (CC BY-NC-ND 4.0).
+https://creativecommons.org/licenses/by-nc-nd/4.0/
+https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode
+
+Copyright (c) COLONOLNUTTY
+"""
+import enum
+
+
+# noinspection PyUnresolvedReferences
+class CommonChoiceOutcome(enum.Int):
+    """ An outcome of the player being given a choice. """
+    CANCEL = 0
+    CHOICE_MADE = 1
+    ERROR = 2
+
+    @staticmethod
+    def is_error_or_cancel(result: 'CommonChoiceOutcome'):
+        """ Determine if an outcome is either Error or Cancel. """
+        return result == CommonChoiceOutcome.ERROR or result == CommonChoiceOutcome.CANCEL
