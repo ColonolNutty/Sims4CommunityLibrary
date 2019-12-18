fca6339b1f7951eb2fa665d1afcd28e03a05f63c
ColonolNutty <ColonolNutty@hotmail.com>
2019-12-18 14:37:24 -0700

    Updates:
    - Moved is_wearing_towel to CommonOutfitUtils
    - CommonSimStateUtils
      - Added is_leaving_zone
      - Added is_hidden
      - Added is_visible

--- Scripts/sims4communitylib/utils/sims/common_sim_state_utils.py
+++ Scripts/sims4communitylib/utils/sims/common_sim_state_utils.py
@@ -5,6 +5,7 @@ https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode
 
 Copyright (c) COLONOLNUTTY
 """
+import services
 from sims.sim_info import SimInfo
 from sims4communitylib.enums.buffs_enum import CommonBuffId
 from sims4communitylib.utils.sims.common_buff_utils import CommonBuffUtils
@@ -13,19 +14,18 @@ from sims4communitylib.utils.sims.common_sim_utils import CommonSimUtils
 
 class CommonSimStateUtils:
     """ Utilities for handling the state of a sim. """
-    @classmethod
-    def is_dying(cls, sim_info: SimInfo) -> bool:
+    @staticmethod
+    def is_dying(sim_info: SimInfo) -> bool:
         """
             Determine if a sim is currently dying.
         """
         return CommonBuffUtils.has_buff(sim_info, CommonBuffId.SIM_IS_DYING)
 
-    @classmethod
-    def is_wearing_towel(cls, sim_info: SimInfo) -> bool:
-        """
-            Determine if a sim is currently wearing a towel.
-        """
-        return CommonBuffUtils.has_buff(sim_info, CommonBuffId.IS_WEARING_TOWEL)
+    @staticmethod
+    def is_wearing_towel(sim_info: SimInfo) -> bool:
+        """ Obsolete: Please use CommonOutfitUtils.is_wearing_towel """
+        from sims4communitylib.utils.cas.common_outfit_utils import CommonOutfitUtils
+        return CommonOutfitUtils.is_wearing_towel(sim_info)
 
     @staticmethod
     def is_in_sunlight(sim_info: SimInfo) -> bool:
@@ -35,3 +35,27 @@ class CommonSimStateUtils:
         from sims4communitylib.utils.common_time_utils import CommonTimeUtils
         sim = CommonSimUtils.get_sim_instance(sim_info)
         return CommonTimeUtils.get_time_service().is_in_sunlight(sim)
+
+    @staticmethod
+    def is_leaving_zone(sim_info: SimInfo) -> bool:
+        """
+            Determine if a sim is currently leaving the zone.
+        """
+        sim = CommonSimUtils.get_sim_instance(sim_info)
+        return sim is not None and services.sim_spawner_service().sim_is_leaving(sim)
+
+    @staticmethod
+    def is_hidden(sim_info: SimInfo) -> bool:
+        """
+            Determine if a sim is hidden.
+        """
+        sim = CommonSimUtils.get_sim_instance(sim_info)
+        sim_id = CommonSimUtils.get_sim_id(sim_info)
+        return sim_id is None or sim is None or services.hidden_sim_service().is_hidden(sim_id) or sim.is_hidden() or sim.opacity == 0
+
+    @staticmethod
+    def is_visible(sim_info: SimInfo) -> bool:
+        """
+            Determine if a sim is visible.
+        """
+        return not CommonSimStateUtils.is_hidden(sim_info)
