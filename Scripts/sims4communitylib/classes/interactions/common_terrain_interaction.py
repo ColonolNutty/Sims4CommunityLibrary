2d521e546fe2ed5e1ae84a188c8a015cadd7b862
ColonolNutty <ColonolNutty@hotmail.com>
2020-01-24 12:33:12 -0700

    Maybe fix CommonTerrainInteraction docs

--- Scripts/sims4communitylib/classes/interactions/common_terrain_interaction.py
+++ Scripts/sims4communitylib/classes/interactions/common_terrain_interaction.py
@@ -15,40 +15,5 @@ class CommonTerrainInteraction(CommonInteraction, TravelMixin, TerrainInteractio
 
     The main use for this class is to create interactions that occur when clicking on the ground.
 
-    :Example:
-
-    .. highlight:: python
-    .. code-block:: python
-
-        # The following is an example interaction that varies when it will display, when it will be hidden, and when it will be disabled with a tooltip.
-        class _ExampleInteraction(CommonTerrainInteraction):
-            @classmethod
-            def on_test(cls, interaction_sim: Sim, interaction_target: Any, interaction_context: InteractionContext, **kwargs) -> TestResult:
-                result = 1 + 1
-                if result == 2:
-                    # Interaction will be displayed, but disabled, it will also have a tooltip that displays on hover with the text "Test Tooltip"
-                    return cls.create_test_result(False, reason="Test Tooltip")
-                    # Alternative way to specify a tooltip with the text "Test Tooltip"
-                    # return cls.create_test_result(False, reason="No Reason", tooltip=CommonLocalizationUtils.create_localized_tooltip("Test Tooltip"))
-                if result == 3:
-                    # Interaction will be hidden completely.
-                    return TestResult.NONE
-                # Interaction will display and be enabled.
-                return TestResult.TRUE
-
-            def on_started(self, interaction_sim: Sim, interaction_target: Any) -> bool:
-                result = True
-                if not result:
-                    return False
-                # Put here what you want the interaction to do as soon as the player clicks it while it is enabled.
-                return True
-
-            def on_cancelled(self, interaction_sim: Sim, interaction_target: Any, finishing_type: FinishingType, cancel_reason_msg: str, **kwargs):
-                result = True
-                if not result:
-                    return False
-                # Put here what you want the interaction to do as soon as the player clicks it while it is enabled.
-                return True
-
     """
     pass
