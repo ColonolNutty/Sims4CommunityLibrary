2234215cb59592ab8e6b34a7476186fd98dfda80
ColonolNutty <ColonolNutty@hotmail.com>
2020-01-25 12:49:03 -0700

    Update basic notification docs

--- Scripts/sims4communitylib/notifications/common_basic_notification.py
+++ Scripts/sims4communitylib/notifications/common_basic_notification.py
@@ -21,8 +21,66 @@ log = CommonLogRegistry.get().register_log(ModInfo.get_identity().name, 'common_
 
 
 class CommonBasicNotification:
-    """Use to create a basic notification.
+    """CommonBasicNotification(\
+        title_identifier,\
+        description_identifier,\
+        title_tokens=(),\
+        description_tokens=(),\
+        urgency=UiDialogNotification.UiDialogNotificationUrgency.DEFAULT,\
+        information_level=UiDialogNotification.UiDialogNotificationLevel.SIM,\
+        expand_behavior=UiDialogNotification.UiDialogNotificationExpandBehavior.USER_SETTING\
+    )
 
+    A basic notification.
+
+    .. note:: Notifications are the messages that appear at the top right in-game.
+
+    .. note:: To see an example dialog, run the command :class:`s4clib_testing.show_basic_notification` in the in-game console.
+
+    :Example usage:
+
+    .. highlight:: python
+    .. code-block:: python
+
+        # Will display a test dialog.
+        def _common_testing_show_basic_notification():
+            # LocalizedStrings within other LocalizedStrings
+            title_tokens = (
+                CommonLocalizationUtils.create_localized_string(
+                    CommonStringId.TESTING_SOME_TEXT_FOR_TESTING,
+                    text_color=CommonLocalizedStringColor.BLUE
+                ),
+            )
+            description_tokens = (
+                CommonLocalizationUtils.create_localized_string(
+                    CommonStringId.TESTING_TEST_TEXT_WITH_SIM_FIRST_AND_LAST_NAME,
+                    tokens=(CommonSimUtils.get_active_sim_info(),),
+                    text_color=CommonLocalizedStringColor.BLUE
+                ),
+            )
+            dialog = CommonBasicNotification(
+                CommonStringId.TESTING_TEST_TEXT_WITH_STRING_TOKEN,
+                CommonStringId.TESTING_TEST_TEXT_WITH_STRING_TOKEN,
+                title_tokens=title_tokens,
+                description_tokens=description_tokens,
+                urgency=UiDialogNotification.UiDialogNotificationUrgency.URGENT
+            )
+            dialog.show()
+
+    :param title_identifier: The title to display in the dialog.
+    :type title_identifier: Union[int, LocalizedString]
+    :param description_identifier: The description to display in the dialog.
+    :type description_identifier: Union[int, LocalizedString]
+    :param title_tokens: Tokens to format into the title.
+    :type title_tokens: Iterator[Any], optional
+    :param description_tokens: Tokens to format into the description.
+    :type description_tokens: Iterator[Any], optional
+    :param urgency: The urgency to which the notification will appear. (URGENT makes it orange)
+    :type urgency: UiDialogNotification.UiDialogNotificationUrgency, optional
+    :param information_level: The information level of the notification.
+    :type information_level: UiDialogNotification.UiDialogNotificationLevel, optional
+    :param expand_behavior: Specify how the notification will expand.
+    :type expand_behavior: UiDialogNotification.UiDialogNotificationExpandBehavior, optional
     """
     def __init__(
         self,
@@ -34,16 +92,6 @@ class CommonBasicNotification:
         information_level: UiDialogNotification.UiDialogNotificationLevel=UiDialogNotification.UiDialogNotificationLevel.SIM,
         expand_behavior: UiDialogNotification.UiDialogNotificationExpandBehavior=UiDialogNotification.UiDialogNotificationExpandBehavior.USER_SETTING
     ):
-        """Create a notification.
-
-        :param title_identifier: A decimal identifier of the title text.
-        :param description_identifier: A decimal identifier of the description text.
-        :param title_tokens: Tokens to format into the title.
-        :param description_tokens: Tokens to format into the description.
-        :param urgency: The urgency to which the notification will appear. (URGENT makes it orange)
-        :param information_level: The information level of the notification.
-        :param expand_behavior: Specify how the notification will expand.
-        """
         self.title = CommonLocalizationUtils.create_localized_string(title_identifier, tokens=tuple(title_tokens))
         self.description = CommonLocalizationUtils.create_localized_string(description_identifier, tokens=tuple(description_tokens))
         self.visual_type = UiDialogNotification.UiDialogNotificationVisualType.INFORMATION
@@ -54,25 +102,41 @@ class CommonBasicNotification:
 
     @CommonExceptionHandler.catch_exceptions(ModInfo.get_identity().name)
     def show(self, icon: IconInfoData=None, secondary_icon: IconInfoData=None):
-        """Show the notification to the player.
+        """show(icon=None, secondary_icon=None)
 
+        Show the notification to the player.
+
+        :param icon: The first icon that will display in the notification.
+        :type icon: IconInfoData, optional
+        :param secondary_icon: The second icon that will display in the notification.
+        :type secondary_icon: IconInfoData, optional
         """
         _notification = self._create_dialog()
         if _notification is None:
             return
 
-        _notification.show_dialog(icon_override=icon, secondary_icon_override=secondary_icon)
+        _notification.show_dialog(
+            icon_override=icon,
+            secondary_icon_override=secondary_icon
+        )
 
     @CommonExceptionHandler.catch_exceptions(ModInfo.get_identity().name, fallback_return=None)
     def _create_dialog(self) -> Union[UiDialogNotification, None]:
-        return UiDialogNotification.TunableFactory().default(None,
-                                                             title=lambda *args, **kwargs: self.title,
-                                                             text=lambda *args, **kwargs: self.description,
-                                                             visual_type=self.visual_type,
-                                                             urgency=self.urgency,
-                                                             information_level=self.information_level,
-                                                             ui_responses=self.ui_responses,
-                                                             expand_behavior=self.expand_behavior)
+        """_create_dialog()
+        Create a dialog for use in :func:``show`.
+
+        .. note:: Override this method with any arguments you want to.
+        """
+        return UiDialogNotification.TunableFactory().default(
+            None,
+            title=lambda *args, **kwargs: self.title,
+            text=lambda *args, **kwargs: self.description,
+            visual_type=self.visual_type,
+            urgency=self.urgency,
+            information_level=self.information_level,
+            ui_responses=self.ui_responses,
+            expand_behavior=self.expand_behavior
+        )
 
 
 @sims4.commands.Command('s4clib_testing.show_basic_notification', command_type=sims4.commands.CommandType.Live)
@@ -82,13 +146,26 @@ def _common_testing_show_basic_notification(_connection: int=None):
 
     try:
         # LocalizedStrings within other LocalizedStrings
-        title_tokens = (CommonLocalizationUtils.create_localized_string(CommonStringId.TESTING_SOME_TEXT_FOR_TESTING, text_color=CommonLocalizedStringColor.BLUE),)
-        description_tokens = (CommonLocalizationUtils.create_localized_string(CommonStringId.TESTING_TEST_TEXT_WITH_SIM_FIRST_AND_LAST_NAME, tokens=(CommonSimUtils.get_active_sim_info(),), text_color=CommonLocalizedStringColor.BLUE),)
-        dialog = CommonBasicNotification(CommonStringId.TESTING_TEST_TEXT_WITH_STRING_TOKEN,
-                                         CommonStringId.TESTING_TEST_TEXT_WITH_STRING_TOKEN,
-                                         title_tokens=title_tokens,
-                                         description_tokens=description_tokens,
-                                         urgency=UiDialogNotification.UiDialogNotificationUrgency.URGENT)
+        title_tokens = (
+            CommonLocalizationUtils.create_localized_string(
+                CommonStringId.TESTING_SOME_TEXT_FOR_TESTING,
+                text_color=CommonLocalizedStringColor.BLUE
+            ),
+        )
+        description_tokens = (
+            CommonLocalizationUtils.create_localized_string(
+                CommonStringId.TESTING_TEST_TEXT_WITH_SIM_FIRST_AND_LAST_NAME,
+                tokens=(CommonSimUtils.get_active_sim_info(),),
+                text_color=CommonLocalizedStringColor.BLUE
+            ),
+        )
+        dialog = CommonBasicNotification(
+            CommonStringId.TESTING_TEST_TEXT_WITH_STRING_TOKEN,
+            CommonStringId.TESTING_TEST_TEXT_WITH_STRING_TOKEN,
+            title_tokens=title_tokens,
+            description_tokens=description_tokens,
+            urgency=UiDialogNotification.UiDialogNotificationUrgency.URGENT
+        )
         dialog.show()
     except Exception as ex:
         log.format_error_with_message('Failed to show a basic notification', exception=ex)
