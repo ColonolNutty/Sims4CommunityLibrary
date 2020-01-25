5ccf1366b8be8f50bba0c5a96d0d2b2be89f2969
ColonolNutty <ColonolNutty@hotmail.com>
2020-01-25 12:31:57 -0700

    Updates:
    - Update ModInfo doc strings

--- Scripts/sims4communitylib/mod_support/common_mod_info.py
+++ Scripts/sims4communitylib/mod_support/common_mod_info.py
@@ -10,13 +10,47 @@ from sims4communitylib.services.common_service import CommonService
 
 
 class CommonModInfo(CommonService):
-    """Information about a mod.
+    """Provide information about your mod.
+
+    For information on what each of the properties represents, see :class:`.CommonModIdentity`
+
+    :Example usage:
+
+    .. highlight:: python
+    .. code-block:: python
+
+        from sims4communitylib.mod_support.common_mod_info import CommonModInfo
+
+        # This is how the sims4communitylib.modinfo.ModInfo implementation works.
+        class ModInfo(CommonModInfo):
+            _FILE_PATH: str = str(__file__)
+
+            @property
+            def _name(self) -> str:
+                return 'Sims4CommunityLib'
+
+            @property
+            def _author(self) -> str:
+                return 'ColonolNutty'
+
+            @property
+            def _base_namespace(self) -> str:
+                return 'sims4communitylib'
+
+            @property
+            def _file_path(self) -> str:
+                return ModInfo._FILE_PATH
 
     """
     @classmethod
     def get_identity(cls) -> CommonModIdentity:
-        """Retrieve an identity for this mod.
+        """The identity of a mod
 
+        .. note:: It contains information about a mod such as Mod Name, Mod Author,\
+            the script base namespace, and the file path to your mod.
+
+        :return: The identity of a mod.
+        :rtype: CommonModIdentity
         """
         identity_property_name = '_MOD_IDENTITY'
         if getattr(cls, identity_property_name, None) is None:
@@ -26,29 +60,16 @@ class CommonModInfo(CommonService):
 
     @property
     def _name(self) -> str:
-        """The name of a mod without spaces.
-
-        """
         raise NotImplementedError('Missing \'{}\'.'.format(self.__class__._name.__name__))
 
     @property
     def _author(self) -> str:
-        """The author of a mod.
-
-        """
         raise NotImplementedError('Missing \'{}\'.'.format(self.__class__._author.__name__))
 
     @property
     def _base_namespace(self) -> str:
-        """The namespace of the ts4script file of a mod.
-        Example: S4CL has a base name of sims4communitylib.
-
-        """
         raise NotImplementedError('Missing \'{}\'.'.format(self.__class__._base_namespace.__name__))
 
     @property
     def _file_path(self) -> str:
-        """The path to the ts4script file of a mod.
-
-        """
         raise NotImplementedError('Missing \'{}\'.'.format(self.__class__._file_path.__name__))
