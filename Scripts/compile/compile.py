36dc910f214be2f627a4c0118c99d54c0813008b
John Dorlus <jsdorlus@elastic.co>
2020-06-11 02:29:45 -0400

    Added conditional to have the right path for linux based machines.

--- Scripts/compile/compile.py
+++ Scripts/compile/compile.py
@@ -5,7 +5,12 @@ https://creativecommons.org/licenses/by/4.0/legalcode
 
 Copyright (c) COLONOLNUTTY
 """
+import os
 from Utilities.compiler import compile_module
 
-compile_module(root='..\\Release\\Sims4CommunityLib', mod_scripts_folder='.', include_folders=('sims4communitylib',), mod_name='sims4communitylib')
-compile_module(root='..\\Release\\Sims4CommunityLibTests', mod_scripts_folder='.', include_folders=('s4cl_tests',), mod_name='sims4communitylib_tests')
+if os.name == 'posix':
+    release_dir = '../Release/Sims4CommunityLib'
+else:
+    release_dir = '..\\Release\\Sims4CommunityLib'
+compile_module(root=release_dir, mod_scripts_folder='.', include_folders=('sims4communitylib',), mod_name='sims4communitylib')
+compile_module(root=f'{release_dir}Tests', mod_scripts_folder='.', include_folders=('s4cl_tests',), mod_name='sims4communitylib_tests')
