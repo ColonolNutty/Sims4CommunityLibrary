ba31a845c8fb623d36a88e25e0a3edecae92820f
ColonolNutty <ColonolNutty@hotmail.com>
2020-06-13 12:39:34 -0600

    Updates:
    - Added kwargs to CommonObjectSpawnUtils.destroy_object
    - Added CommonObjectSpawnUtils.schedule_object_for_destroy
    - Changed the default value for first_name and last_name in
    CommonSimSpawnUtils.create_human_sim_info to an empty string
    - Update docstrings for CommonObjectSpawnUtils and CommonSimSpawnUtils
    - Added kwargs to CommonSimSpawnUtils.spawn_sim
    - Added kwargs to CommonSimSpawnUtils.spawn_sim_at_active_sim_location
    - CommonSimSpawnUtils.spawn_sim_at_active_sim_location will now use the
    active Sims position if their location is not found.
    - Added source kwarg and kwargs to CommonSimSpawnUtils.despawn_sim
    - Added CommonSimSpawnUtils.schedule_sim_for_despawn
    - Added source kwargs and kwargs to CommonSimSpawnUtils.delete_sim

--- Scripts/sims4communitylib/utils/sims/common_sim_spawn_utils.py
+++ Scripts/sims4communitylib/utils/sims/common_sim_spawn_utils.py
@@ -5,7 +5,7 @@ https://creativecommons.org/licenses/by/4.0/legalcode
 
 Copyright (c) COLONOLNUTTY
 """
-from typing import Union, Tuple
+from typing import Union, Tuple, Callable
 
 from protocolbuffers.Math_pb2 import Vector3
 from routing import Location
@@ -27,8 +27,8 @@ class CommonSimSpawnUtils:
     def create_human_sim_info(
         gender: Gender=None,
         age: Age=None,
-        first_name: str=None,
-        last_name: str=None,
+        first_name: str='',
+        last_name: str='',
         trait_ids: Tuple[int]=(),
         household: Household=None,
         source: str='testing'
@@ -37,8 +37,8 @@ class CommonSimSpawnUtils:
             gender=None,\
             age=None,\
             species=None,\
-            first_name=None,\
-            last_name=None,\
+            first_name='',\
+            last_name='',\
             trait_ids=(),\
             household=None,\
             source='testing'\
@@ -46,19 +46,19 @@ class CommonSimSpawnUtils:
 
         Create SimInfo for a Human Sim.
 
-        :param gender: The Gender of the created Sim.
+        :param gender: The Gender of the created Sim. Default is None.
         :type gender: Gender, optional
-        :param age: The Age of the created Sim.
+        :param age: The Age of the created Sim. Default is None.
         :type age: Age, optional
-        :param first_name: The First Name of the created Sim.
+        :param first_name: The First Name of the created Sim. Default is an empty string.
         :type first_name: str, optional
-        :param last_name: The Last Name of the created Sim.
+        :param last_name: The Last Name of the created Sim. Default is an empty string.
         :type last_name: str, optional
-        :param trait_ids: The decimal identifiers of the Traits to add to the created Sim.
+        :param trait_ids: The decimal identifiers of the Traits to add to the created Sim. Default is an empty collection.
         :type trait_ids: Tuple[int], optional
-        :param household: The household to place the created Sim in. If None, the Sim will be placed in a hidden household.
+        :param household: The household to place the created Sim in. If None, the Sim will be placed in a hidden household. Default is None.
         :type household: Household, optional
-        :param source: The reason for the Sims creation.
+        :param source: The reason for the Sims creation. Default is 'testing'.
         :type source: str, optional
         :return: The SimInfo of the created Sim or None if the Sim failed to be created.
         :rtype: SimInfo
@@ -72,10 +72,12 @@ class CommonSimSpawnUtils:
         return sim_info_list[0]
 
     @staticmethod
-    def spawn_sim(sim_info: SimInfo, location: Location=None, position: Vector3=None) -> bool:
-        """spawn_sim(sim_info, location=None, position=None)
+    def spawn_sim(sim_info: SimInfo, location: Location=None, position: Vector3=None, **kwargs) -> bool:
+        """spawn_sim(sim_info, location=None, position=None, **kwargs)
 
         Spawn a Sim.
+
+        ..note:: Do not provide sim_position or sim_location in kwargs as they are already specified as location and position.
 
         :param sim_info: The Sim to Spawn.
         :type sim_info: SimInfo
@@ -87,17 +89,17 @@ class CommonSimSpawnUtils:
         :rtype: bool
         """
         try:
-            SimSpawner.spawn_sim(sim_info, sim_location=location, sim_position=position)
+            SimSpawner.spawn_sim(sim_info, sim_location=location, sim_position=position, **kwargs)
         except Exception as ex:
             CommonExceptionHandler.log_exception(ModInfo.get_identity(), 'Failed to spawn Sim with SimInfo \'{}\' at location \'{}\'.'.format(sim_info, location), exception=ex)
             return False
         return True
 
     @staticmethod
-    def spawn_sim_at_active_sim_location(sim_info: SimInfo) -> bool:
-        """spawn_sim_at_active_sim_location(sim_info)
+    def spawn_sim_at_active_sim_location(sim_info: SimInfo, **kwargs) -> bool:
+        """spawn_sim_at_active_sim_location(sim_info, **kwargs)
 
-        Spawn a Sim at the position of the Active Sim.
+        Spawn a Sim at the location of the Active Sim.
 
         :param sim_info: The Sim to Spawn.
         :type sim_info: SimInfo
@@ -107,17 +109,24 @@ class CommonSimSpawnUtils:
         from sims4communitylib.utils.sims.common_sim_location_utils import CommonSimLocationUtils
         from sims4communitylib.utils.sims.common_sim_utils import CommonSimUtils
         active_sim_info = CommonSimUtils.get_active_sim_info()
-        return CommonSimSpawnUtils.spawn_sim(sim_info, location=CommonSimLocationUtils.get_location(active_sim_info))
+        active_location = CommonSimLocationUtils.get_location(active_sim_info)
+        active_position = None
+        if active_location is None:
+            active_position = CommonSimLocationUtils.get_position(active_sim_info)
+        return CommonSimSpawnUtils.spawn_sim(sim_info, location=active_location, position=active_position, **kwargs)
 
     @staticmethod
-    def despawn_sim(sim_info: SimInfo, cause: str=None) -> bool:
-        """despawn_sim(sim_info, cause=None)
+    @CommonExceptionHandler.catch_exceptions(ModInfo.get_identity(), fallback_return=False)
+    def despawn_sim(sim_info: SimInfo, source: str=None, cause: str=None, **kwargs) -> bool:
+        """despawn_sim(sim_info, source=None, cause=None, **kwargs)
 
         Despawn a Sim.
 
         :param sim_info: The Sim to despawn.
         :type sim_info: SimInfo
-        :param cause: The reason for the despawn.
+        :param source: The source of the destruction. Default is None.
+        :type source: str, optional
+        :param cause: The cause of the destruction. Default is None.
         :type cause: str, optional
         :return: True, if the Sim was despawn successfully. False, if not.
         :rtype: bool
@@ -128,24 +137,57 @@ class CommonSimSpawnUtils:
         sim = CommonSimUtils.get_sim_instance(sim_info)
         if sim is None:
             return True
-        cause = cause or 'Sim destroyed.'
-        sim.destroy(cause=cause)
+        cause = cause or 'Sim despawned.'
+        sim.destroy(source=source, cause=cause, **kwargs)
         return True
 
     @staticmethod
-    def delete_sim(sim_info: SimInfo, cause: str=None) -> bool:
-        """delete_sim(sim_info, cause=None)
+    @CommonExceptionHandler.catch_exceptions(ModInfo.get_identity(), fallback_return=False)
+    def schedule_sim_for_despawn(sim_info: SimInfo, source: str=None, cause: str=None, on_despawn: Callable[[], None]=None, **kwargs) -> bool:
+        """schedule_sim_for_despawn(sim_info, source=None, cause=None, on_despawn=None, **kwargs)
+
+        Schedule a Sim to be despawned.
+
+        :param sim_info: The Sim to despawn.
+        :type sim_info: SimInfo
+        :param source: The source of the destruction. Default is None.
+        :type source: str, optional
+        :param cause: The cause of the destruction. Default is None.
+        :type cause: str, optional
+        :param on_despawn: A callback that occurs after the Sim is despawned. Default is None.
+        :type on_despawn: Callable[[], None], optional
+        :return: True, if the Object was successfully scheduled for destruction. False, if not.
+        :rtype: bool
+        """
+        if sim_info is None:
+            return False
+        from sims4communitylib.utils.sims.common_sim_utils import CommonSimUtils
+        sim = CommonSimUtils.get_sim_instance(sim_info)
+        if sim is None:
+            if on_despawn is not None:
+                on_despawn()
+            return True
+        cause = cause or 'Sim despawned.'
+        sim.schedule_destroy_asap(post_delete_func=on_despawn, source=source, cause=cause, **kwargs)
+        return True
+
+    @staticmethod
+    @CommonExceptionHandler.catch_exceptions(ModInfo.get_identity(), fallback_return=False)
+    def delete_sim(sim_info: SimInfo, source: str=None, cause: str=None, **kwargs) -> bool:
+        """delete_sim(sim_info, source=None, cause=None, **kwargs)
 
         Delete a Sim.
 
         :param sim_info: The Sim to delete.
         :type sim_info: SimInfo
-        :param cause: The reason for the deletion.
+        :param source: The source of the destruction. Default is None.
+        :type source: str, optional
+        :param cause: The cause of the destruction. Default is None.
         :type cause: str, optional
         :return: True, if the Sim was deleted successfully. False, if not.
         :rtype: bool
         """
-        if not CommonSimSpawnUtils.despawn_sim(sim_info, cause=cause):
+        if not CommonSimSpawnUtils.despawn_sim(sim_info, source=source, cause=cause, **kwargs):
             return False
         sim_info.remove_permanently()
         return True
