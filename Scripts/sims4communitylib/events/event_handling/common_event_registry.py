abd2e40e4f58d79bf26d80dd9a82480c462e71f3
ColonolNutty <ColonolNutty@hotmail.com>
2020-09-29 10:47:21 -0600

    Updates:
    - Remove dispatch_async from CommonEvent Registry because it was not
    working correctly and would lock up the game. (Doing this before it is
    included in the next release)

--- Scripts/sims4communitylib/events/event_handling/common_event_registry.py
+++ Scripts/sims4communitylib/events/event_handling/common_event_registry.py
@@ -5,8 +5,6 @@ https://creativecommons.org/licenses/by/4.0/legalcode
 
 Copyright (c) COLONOLNUTTY
 """
-from queue import Queue
-from threading import Thread
 from typing import List, Callable, Any, Union
 from sims4communitylib.events.event_handling.common_event import CommonEvent
 from sims4communitylib.events.event_handling.common_event_handler import CommonEventHandler
@@ -14,8 +12,6 @@ from sims4communitylib.exceptions.common_exceptions_handler import CommonExcepti
 from sims4communitylib.mod_support.mod_identity import CommonModIdentity
 from sims4communitylib.modinfo import ModInfo
 from sims4communitylib.services.common_service import CommonService
-from sims4communitylib.utils.common_injection_utils import CommonInjectionUtils
-from zone import Zone
 
 
 class CommonEventRegistry(CommonService):
@@ -25,15 +21,6 @@ class CommonEventRegistry(CommonService):
 
     def __init__(self) -> None:
         self._event_handlers: List[CommonEventHandler] = []
-        self._event_queue = Queue()
-        self._running = True
-        self._number_of_worker_threads = 5
-        self._worker_threads = list()
-        for i in range(self._number_of_worker_threads):
-            worker = Thread(target=self._work_the_queue, args=(self._event_queue,))
-            worker.daemon = True
-            self._worker_threads.append(worker)
-            worker.start()
 
     @staticmethod
     def handle_events(mod_identifier: Union[str, CommonModIdentity]) -> Callable[[Callable[[CommonEvent], bool]], Callable[[CommonEvent], bool]]:
@@ -57,33 +44,6 @@ class CommonEventRegistry(CommonService):
         event_handler = CommonEventHandler(mod_identifier, event_function)
         self._event_handlers.append(event_handler)
 
-    def start(self) -> None:
-        """start()
-
-        Start dispatching events that are in the queue.
-        """
-        self._running = True
-        for worker in self._worker_threads:
-            worker.start()
-
-    def stop(self) -> None:
-        """stop()
-
-        Stop dispatching events that are in the queue.
-        """
-        self._running = False
-
-    def _should_continue_running(self) -> bool:
-        return self._running
-
-    def _work_the_queue(self, event_queue: Queue):
-        while self._should_continue_running():
-            (event, on_complete) = event_queue.get()
-            result = self._dispatch(event)
-            if on_complete is not None:
-                on_complete(result)
-            event_queue.task_done()
-
     def dispatch(self, event: CommonEvent) -> bool:
         """dispatch(event)
 
@@ -97,21 +57,6 @@ class CommonEventRegistry(CommonService):
         :rtype: bool
         """
         return self._dispatch(event)
-
-    def dispatch_async(self, event: CommonEvent, on_complete: Callable[[bool], Any]=None) -> bool:
-        """dispatch_async(event, on_complete=None)
-
-        Dispatch an event asynchronously and perform a callback upon it's completion.
-
-        :param event: An instance of an Event to dispatch to listeners.
-        :type event: CommonEvent
-        :param on_complete: If provided, when the event has finished being dispatched, this callback will be invoked with the result. Default is None.
-        :type on_complete: Callable[[bool], Any], optional
-        :return: True will always be returned.
-        :rtype: bool
-        """
-        self._event_queue.put((event, on_complete))
-        return True
 
     def _dispatch(self, event: CommonEvent) -> bool:
         event_handlers = list(self._event_handlers)
@@ -130,18 +75,4 @@ class CommonEventRegistry(CommonService):
         except Exception as ex:
             CommonExceptionHandler.log_exception(ModInfo.get_identity(), 'Failed to dispatch event \'{}\''.format(event), exception=ex)
             return False
-        return result
-
-    @staticmethod
-    @CommonInjectionUtils.inject_safely_into(ModInfo.get_identity().name, Zone, Zone.load_zone.__name__)
-    def _common_stop_dispatching_events_on_zone_load(original, self: Zone, *args, **kwargs):
-        CommonEventRegistry().start()
-        result = original(self, *args, **kwargs)
-        return result
-
-    @staticmethod
-    @CommonInjectionUtils.inject_safely_into(ModInfo.get_identity().name, Zone, Zone.on_teardown.__name__)
-    def _common_stop_dispatching_events_on_zone_teardown(original, self: Zone, client):
-        CommonEventRegistry().stop()
-        result = original(self, client)
         return result
--- Scripts/sims4communitylib/events/sim/common_sim_event_dispatcher.py
+++ Scripts/sims4communitylib/events/sim/common_sim_event_dispatcher.py
@@ -45,23 +45,23 @@ class CommonSimEventDispatcherService(CommonService):
 
     def _on_sim_spawned(self, sim_info: SimInfo, *_, **__):
         from sims4communitylib.utils.sims.common_sim_utils import CommonSimUtils
-        return CommonEventRegistry.get().dispatch_async(S4CLSimSpawnedEvent(CommonSimUtils.get_sim_info(sim_info)))
+        return CommonEventRegistry.get().dispatch(S4CLSimSpawnedEvent(CommonSimUtils.get_sim_info(sim_info)))
 
     def _on_sim_change_age(self, sim_info: SimInfo, new_age: Age, current_age: Age):
         from sims4communitylib.utils.sims.common_sim_utils import CommonSimUtils
-        return CommonEventRegistry.get().dispatch_async(S4CLSimChangedAgeEvent(CommonSimUtils.get_sim_info(sim_info), current_age, new_age))
+        return CommonEventRegistry.get().dispatch(S4CLSimChangedAgeEvent(CommonSimUtils.get_sim_info(sim_info), current_age, new_age))
 
     def _on_sim_add_occult_type(self, occult_tracker: OccultTracker, occult_type: OccultType):
         sim_info = occult_tracker._sim_info
-        return CommonEventRegistry.get().dispatch_async(S4CLSimAddedOccultTypeEvent(sim_info, occult_type, occult_tracker))
+        return CommonEventRegistry.get().dispatch(S4CLSimAddedOccultTypeEvent(sim_info, occult_type, occult_tracker))
 
     def _on_sim_change_occult_type(self, occult_tracker: OccultTracker, occult_type: OccultType, *_, **__):
         sim_info = occult_tracker._sim_info
-        return CommonEventRegistry.get().dispatch_async(S4CLSimChangedOccultTypeEvent(sim_info, occult_type, occult_tracker))
+        return CommonEventRegistry.get().dispatch(S4CLSimChangedOccultTypeEvent(sim_info, occult_type, occult_tracker))
 
     def _on_sim_remove_occult_type(self, occult_tracker: OccultTracker, occult_type: OccultType):
         sim_info = occult_tracker._sim_info
-        return CommonEventRegistry.get().dispatch_async(S4CLSimRemovedOccultTypeEvent(sim_info, occult_type, occult_tracker))
+        return CommonEventRegistry.get().dispatch(S4CLSimRemovedOccultTypeEvent(sim_info, occult_type, occult_tracker))
 
 
 @CommonInjectionUtils.inject_safely_into(ModInfo.get_identity().name, SimInfo, SimInfo.__init__.__name__)
