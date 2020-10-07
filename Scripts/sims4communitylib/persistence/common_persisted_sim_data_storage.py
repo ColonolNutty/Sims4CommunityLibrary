c129275859de8f3307f821e3615cc4984d6de98a
ColonolNutty <ColonolNutty@hotmail.com>
2020-10-07 14:21:51 -0600

    Updates:
    - Add the ability to persist data that will be available upon loading
    the game again.

---
+++ Scripts/sims4communitylib/persistence/common_persisted_sim_data_storage.py
@@ -0,0 +1,129 @@
+"""
+The Sims 4 Community Library is licensed under the Creative Commons Attribution 4.0 International public license (CC BY 4.0).
+https://creativecommons.org/licenses/by/4.0/
+https://creativecommons.org/licenses/by/4.0/legalcode
+
+Copyright (c) COLONOLNUTTY
+"""
+import sys
+from typing import Dict, Any, Tuple, Type
+
+from sims.sim_info import SimInfo
+from sims4communitylib.mod_support.mod_identity import CommonModIdentity
+from sims4communitylib.persistence.data_management.common_data_manager import CommonDataManager
+from sims4communitylib.persistence.data_stores.common_data_store import CommonDataStore
+from sims4communitylib.persistence.common_sim_data_storage import CommonSimDataStorage
+from sims4communitylib.persistence.data_stores.common_sim_data_store import CommonSimDataStore
+from sims4communitylib.utils.sims.common_sim_utils import CommonSimUtils
+
+
+class CommonPersistedSimDataStorage(CommonSimDataStorage):
+    """CommonPersistedSimDataStorage(sim_info)
+
+    A wrapper for Sim instances that allows storing of data and persisting it between saves.
+
+    .. warning:: Data stored within will be persisted for a Save even when closing and reopening the game!
+    .. warning:: DO NOT CREATE THIS CLASS DIRECTLY, IT IS ONLY MEANT TO INHERIT FROM!
+
+    :Example usage:
+
+    .. highlight:: python
+    .. code-block:: python
+
+        # Inherit from CommonPersistedSimDataStorage
+        class ExamplePersistedSimDataStorage(CommonPersistedSimDataStorage):
+            @classmethod
+            def get_mod_identity(cls) -> CommonModIdentity:
+                # !!!Override with the CommonModIdentity of your own mod!!!
+                from sims4communitylib.modinfo import ModInfo
+                return ModInfo.get_identity()
+
+            @property
+            def example_property_one(self) -> bool:
+                # Could also be written self.get_data(default=False, key='example_property_one') and it would do the same thing.
+                return self.get_data(default=False)
+
+            @example_property_one.setter
+            def example_property_one(self, value: bool):
+                # Could also be written self.set_data(value, key='example_property_one') and it would do the same thing.
+                self.set_data(value)
+
+    :param sim_info: The SimInfo of a Sim.
+    :type sim_info: SimInfo
+    """
+    def __init__(self, sim_info: SimInfo):
+        super().__init__(sim_info)
+        if self.__class__.__name__ is CommonPersistedSimDataStorage.__name__:
+            raise RuntimeError('{} cannot be created directly. You must inherit from it to create an instance of it.'.format(self.__class__.__name__))
+        from sims4communitylib.persistence.data_management.common_data_manager_registry import CommonDataManagerRegistry
+        self._data_manager_registry = CommonDataManagerRegistry()
+        self.__data_manager: CommonDataManager = None
+        self._data = self._load_persisted_data()
+
+    # noinspection PyMissingOrEmptyDocstring,PyMethodParameters
+    @classmethod
+    def get_mod_identity(cls) -> CommonModIdentity:
+        return super().get_mod_identity()
+
+    # noinspection PyMissingOrEmptyDocstring
+    @classmethod
+    def get_log_identifier(cls) -> str:
+        return 'common_persisted_sim_data_storage'
+
+    @property
+    def data_store_type(self) -> Type[CommonDataStore]:
+        """data_store_type()
+
+        The type of data store used for saving and loading data.
+
+        :return: The type of the data store to use when saving and loading data.
+        :rtype: Type[CommonDataStore]
+        """
+        return CommonSimDataStore
+
+    @property
+    def whitelist_property_names(self) -> Tuple[str]:
+        """
+        If a property is within this list, it will be persisted when saving. By default all properties are whitelisted.
+
+        :return: A collection of property names.
+        :rtype: Tuple[str]
+        """
+        return tuple(self._data.keys())
+
+    @property
+    def blacklist_property_names(self) -> Tuple[str]:
+        """
+        If a property is within this list, it will not be persisted when saving. By default no properties are blacklisted.
+
+        :return: A collection of property names.
+        :rtype: Tuple[str]
+        """
+        return tuple()
+
+    @property
+    def _data_manager(self) -> CommonDataManager:
+        if self.__data_manager is None:
+            self.__data_manager = self._data_manager_registry.locate_data_manager(self.mod_identity)
+            if self.__data_manager is None:
+                raise RuntimeError('Failed to locate a data manager for {}, maybe you forgot to register one?'.format(self.mod_identity.name))
+        return self.__data_manager
+
+    # noinspection PyMissingOrEmptyDocstring
+    def set_data(self, value: Any, key: str=None):
+        key = key or str(sys._getframe(1).f_code.co_name)
+        super().set_data(value, key=key)
+        self._save_persisted_data()
+
+    def _save_persisted_data(self) -> None:
+        data_to_save = dict()
+        for data_property_name in self._data.keys():
+            if data_property_name not in self.blacklist_property_names or data_property_name in self.blacklist_property_names:
+                continue
+            data_to_save[data_property_name] = self._data[data_property_name]
+        sim_id = CommonSimUtils.get_sim_id(self.sim_info)
+        self._data_manager.get_data_store_by_type(self.data_store_type).set_value_by_key(str(sim_id), data_to_save)
+
+    def _load_persisted_data(self) -> Dict[str, Any]:
+        sim_id = CommonSimUtils.get_sim_id(self.sim_info)
+        return self._data_manager.get_data_store_by_type(self.data_store_type).get_value_by_key(str(sim_id)).copy()
---
+++ Scripts/sims4communitylib/persistence/data_stores/common_game_object_data_store.py
@@ -0,0 +1,31 @@
+"""
+The Sims 4 Community Library is licensed under the Creative Commons Attribution 4.0 International public license (CC BY 4.0).
+https://creativecommons.org/licenses/by/4.0/
+https://creativecommons.org/licenses/by/4.0/legalcode
+
+Copyright (c) COLONOLNUTTY
+"""
+from typing import Dict, Any
+
+from sims4communitylib.persistence.data_stores.common_data_store import CommonDataStore
+
+
+class CommonGameObjectDataStore(CommonDataStore):
+    """ A store of Game Object Data. """
+
+    # noinspection PyMissingOrEmptyDocstring
+    @classmethod
+    def get_identifier(cls) -> str:
+        return 'game_object_data'
+
+    @property
+    def _version(self) -> int:
+        return 1
+
+    @property
+    def _default_data(self) -> Dict[str, Any]:
+        return dict()
+
+    # noinspection PyMissingOrEmptyDocstring
+    def get_default_value_by_key(self, key: str) -> Any:
+        return dict()
