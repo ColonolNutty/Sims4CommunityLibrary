d72000c51ddba45968d996be11d26f7e7e4e3cdf
ColonolNutty <ColonolNutty@hotmail.com>
2020-11-18 13:59:44 -0700

    Updates:
    - Added a fix when trying to remove traits that pointed out a bug in the
    vanilla code.

---
+++ Scripts/sims4communitylib/_vanilla_fixes/_sim_knowledge.py
@@ -0,0 +1,10 @@
+from relationships.sim_knowledge import SimKnowledge
+from sims4communitylib.modinfo import ModInfo
+from sims4communitylib.utils.common_injection_utils import CommonInjectionUtils
+
+if hasattr(SimKnowledge, 'remove_known_trait'):
+    @CommonInjectionUtils.inject_safely_into(ModInfo.get_identity(), SimKnowledge, SimKnowledge.remove_known_trait.__name__, handle_exceptions=False)
+    def _common_fix_vanilla_remove_known_trait_not_checking_known_traits(original, self: SimKnowledge, trait, *_, **__):
+        if self._known_traits is not None and trait not in self._known_traits:
+            return
+        return original(self, trait, *_, **__)
