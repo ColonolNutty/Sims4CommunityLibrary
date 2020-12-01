6e0725e9cfb39a9e963c465c7b63e50b9f23e9b8
ColonolNutty <ColonolNutty@hotmail.com>
2020-12-01 12:56:57 -0700

    Minor warning fixes

--- Scripts/sims4communitylib/_vanilla_fixes/_sim_knowledge.py
+++ Scripts/sims4communitylib/_vanilla_fixes/_sim_knowledge.py
@@ -1,3 +1,10 @@
+"""
+The Sims 4 Community Library is licensed under the Creative Commons Attribution 4.0 International public license (CC BY 4.0).
+https://creativecommons.org/licenses/by/4.0/
+https://creativecommons.org/licenses/by/4.0/legalcode
+
+Copyright (c) COLONOLNUTTY
+"""
 from relationships.sim_knowledge import SimKnowledge
 from sims4communitylib.modinfo import ModInfo
 from sims4communitylib.utils.common_injection_utils import CommonInjectionUtils
