a676cd3918e988e047df21d56e871a7325f68efb
ColonolNutty <ColonolNutty@hotmail.com>
2021-01-17 12:34:05 -0700

    Updates:
    - Added CommonAvailableFor

---
+++ Scripts/sims4communitylib/classes/calculations/__init__.py
@@ -0,0 +1,8 @@
+"""
+This file is part of the Custom Slider Framework licensed under the Creative Commons Attribution-NoDerivatives 4.0 International public license (CC BY-ND 4.0).
+
+https://creativecommons.org/licenses/by-nd/4.0/
+https://creativecommons.org/licenses/by-nd/4.0/legalcode
+
+Copyright (c) COLONOLNUTTY
+"""
