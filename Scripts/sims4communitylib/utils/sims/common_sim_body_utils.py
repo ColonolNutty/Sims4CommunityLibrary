99814e964f42e3bb4ef9f917a0785d21beae3734
ColonolNutty <ColonolNutty@hotmail.com>
2024-05-01 13:09:05 -0700

    Updates:
    - Added functions to control fit and fat levels

--- Scripts/sims4communitylib/utils/sims/common_sim_body_utils.py
+++ Scripts/sims4communitylib/utils/sims/common_sim_body_utils.py
@@ -83,14 +83,78 @@ class CommonSimBodyUtils:
             return 0, 0
         return wading_interval.lower_bound, wading_interval.upper_bound
 
+    @classmethod
+    def set_fit_level(cls, sim_info: SimInfo, level: float):
+        """set_fit_level(sim_info, level)
+
+        Set the Fit level of a Sim.
+
+        :param sim_info: The Sim to modify.
+        :type sim_info: SimInfo
+        :param level: The Fit level to set the Sim to.
+        :type level: float
+        """
+        sim = CommonSimUtils.get_sim_instance(sim_info)
+        from objects.components.consumable_component import ConsumableComponent
+        sim.commodity_tracker.set_value(ConsumableComponent.FIT_COMMODITY, level)
+        sim_info._sim_ref().needs_fitness_update = True
+        sim_info.update_fitness_state()
+
+    @classmethod
+    def get_fit_level(cls, sim_info: SimInfo) -> float:
+        """get_fit_level(sim_info)
+
+        Get the Fit level of a Sim.
+
+        :param sim_info: The Sim to modify.
+        :type sim_info: SimInfo
+        :return: The Fit level of the Sim.
+        :rtype: float
+        """
+        sim = CommonSimUtils.get_sim_instance(sim_info)
+        from objects.components.consumable_component import ConsumableComponent
+        return sim.commodity_tracker.get_value(ConsumableComponent.FIT_COMMODITY)
+
+    @classmethod
+    def set_fat_level(cls, sim_info: SimInfo, level: float):
+        """set_fat_level(sim_info, level)
+
+        Set the Fat level of a Sim.
+
+        :param sim_info: The Sim to modify.
+        :type sim_info: SimInfo
+        :param level: The Fat level to set the Sim to.
+        :type level: float
+        """
+        sim = CommonSimUtils.get_sim_instance(sim_info)
+        from objects.components.consumable_component import ConsumableComponent
+        sim.commodity_tracker.set_value(ConsumableComponent.FAT_COMMODITY, level)
+        sim_info._sim_ref().needs_fitness_update = True
+        sim_info.update_fitness_state()
+
+    @classmethod
+    def get_fat_level(cls, sim_info: SimInfo) -> float:
+        """get_fat_level(sim_info)
+
+        Get the Fat level of a Sim.
+
+        :param sim_info: The Sim to modify.
+        :type sim_info: SimInfo
+        :return: The Fat level of the Sim.
+        :rtype: float
+        """
+        sim = CommonSimUtils.get_sim_instance(sim_info)
+        from objects.components.consumable_component import ConsumableComponent
+        return sim.commodity_tracker.get_value(ConsumableComponent.FAT_COMMODITY)
+
 
 @CommonConsoleCommand(
     ModInfo.get_identity(),
     's4cl.set_fit',
-    'Set the fit level of a Sim.',
+    'Set the Fit level of a Sim.',
     command_arguments=(
-        CommonConsoleCommandArgument('level', 'Number', 'The level of fit to set on the Sim.', is_optional=False),
-        CommonConsoleCommandArgument('sim_info', 'Name or ID', 'The Sim to update.', is_optional=True, default_value='Active Sim'),
+        CommonConsoleCommandArgument('level', 'Number', 'The Fit level to set the Sim to.', is_optional=False),
+        CommonConsoleCommandArgument('sim_info', 'Name or ID', 'The name or ID of the Sim to modify.', is_optional=True, default_value='Active Sim'),
     ),
     command_aliases=(
         's4cl.set_fit_level',
@@ -100,22 +164,18 @@ def _common_set_fit_level(output: CommonConsoleCommandOutput, level: float, sim_
     if sim_info is None:
         output(f'No Sim found with {sim_info}')
         return
-    output(f'Setting fit level of {sim_info} to {level}')
-    sim = CommonSimUtils.get_sim_instance(sim_info)
-    from objects.components.consumable_component import ConsumableComponent
-    sim.commodity_tracker.set_value(ConsumableComponent.FIT_COMMODITY, level)
-    sim_info._sim_ref().needs_fitness_update = True
-    sim_info.update_fitness_state()
+    output(f'Setting Fit level of {sim_info} to {level}.')
+    CommonSimBodyUtils.set_fit_level(sim_info, level)
     output('Done')
 
 
 @CommonConsoleCommand(
     ModInfo.get_identity(),
     's4cl.set_fat',
-    'Set the fat level of a Sim.',
+    'Set the Fat level of a Sim.',
     command_arguments=(
-        CommonConsoleCommandArgument('level', 'Number', 'The level of fat to set on the Sim.', is_optional=False),
-        CommonConsoleCommandArgument('sim_info', 'Name or ID', 'The Sim to update.', is_optional=True, default_value='Active Sim'),
+        CommonConsoleCommandArgument('level', 'Number', 'The Fat level to set the Sim to.', is_optional=False),
+        CommonConsoleCommandArgument('sim_info', 'Name or ID', 'The name or ID of the Sim to modify.', is_optional=True, default_value='Active Sim'),
     ),
     command_aliases=(
         's4cl.set_fat_level',
@@ -125,10 +185,6 @@ def _common_set_fat_level(output: CommonConsoleCommandOutput, level: float, sim_
     if sim_info is None:
         output(f'No Sim found with {sim_info}')
         return
-    output(f'Setting fit level of {sim_info} to {level}')
-    sim = CommonSimUtils.get_sim_instance(sim_info)
-    from objects.components.consumable_component import ConsumableComponent
-    sim.commodity_tracker.set_value(ConsumableComponent.FAT_COMMODITY, level)
-    sim_info._sim_ref().needs_fitness_update = True
-    sim_info.update_fitness_state()
+    output(f'Setting Fat level of {sim_info} to {level}.')
+    CommonSimBodyUtils.set_fat_level(sim_info, level)
     output('Done')
